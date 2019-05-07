cbd9bb0e8c82844ad7af4bbf1754d987d2ac0aac
Minato <xuanha.tlu9x@gmail.com>
2019-05-08 00:03:15 +0700

    first commit

---
+++ src/main/java/com/thanglongedu/learnsql/service/dto/CategoryTypeDTO.java
@@ -0,0 +1,121 @@
+package com.thanglongedu.learnsql.service.dto;
+import java.time.Instant;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A DTO for the CategoryType entity.
+ */
+public class CategoryTypeDTO implements Serializable {
+
+    private Long id;
+
+    @NotNull
+    private String nameCategoryType;
+
+    private String description;
+
+    @NotNull
+    private Instant createdDate;
+
+    @NotNull
+    private Integer createdBy;
+
+    @NotNull
+    private Instant updatedDate;
+
+    @NotNull
+    private Integer updatedBy;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameCategoryType() {
+        return nameCategoryType;
+    }
+
+    public void setNameCategoryType(String nameCategoryType) {
+        this.nameCategoryType = nameCategoryType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Instant getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Instant updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        CategoryTypeDTO categoryTypeDTO = (CategoryTypeDTO) o;
+        if (categoryTypeDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), categoryTypeDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryTypeDTO{" +
+            "id=" + getId() +
+            ", nameCategoryType='" + getNameCategoryType() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", createdDate='" + getCreatedDate() + "'" +
+            ", createdBy=" + getCreatedBy() +
+            ", updatedDate='" + getUpdatedDate() + "'" +
+            ", updatedBy=" + getUpdatedBy() +
+            "}";
+    }
+}
---
+++ src/main/webapp/app/entities/type-content/type-content.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { JhiLanguageService } from 'ng-jhipster';
+import { JhiLanguageHelper } from 'app/core';
+
+import { LearnSqlSharedModule } from 'app/shared';
+import {
+    TypeContentComponent,
+    TypeContentDetailComponent,
+    TypeContentUpdateComponent,
+    TypeContentDeletePopupComponent,
+    TypeContentDeleteDialogComponent,
+    typeContentRoute,
+    typeContentPopupRoute
+} from './';
+
+const ENTITY_STATES = [...typeContentRoute, ...typeContentPopupRoute];
+
+@NgModule({
+    imports: [LearnSqlSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    declarations: [
+        TypeContentComponent,
+        TypeContentDetailComponent,
+        TypeContentUpdateComponent,
+        TypeContentDeleteDialogComponent,
+        TypeContentDeletePopupComponent
+    ],
+    entryComponents: [TypeContentComponent, TypeContentUpdateComponent, TypeContentDeleteDialogComponent, TypeContentDeletePopupComponent],
+    providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class LearnSqlTypeContentModule {
+    constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
+        this.languageHelper.language.subscribe((languageKey: string) => {
+            if (languageKey !== undefined) {
+                this.languageService.changeLanguage(languageKey);
+            }
+        });
+    }
+}
